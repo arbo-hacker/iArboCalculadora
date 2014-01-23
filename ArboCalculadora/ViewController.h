685cb6127d6792b9a8e97caed6d90cf633c5b25b
Alejandro Barreto <arbo.com.ve@gmail.com>
2014-01-22 20:51:13 -0430

    Se sube version que ya es capaz de agregar dígitos

--- ArboCalculadora/ViewController.h
+++ ArboCalculadora/ViewController.h
@@ -8,6 +8,33 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    UILabel *calculadoraText_;
+}
+
+@property (nonatomic) IBOutlet UILabel *calculadoraText;
+
+-(IBAction)zeroButton:(id)sender;
+-(IBAction)oneButton:(id)sender;
+-(IBAction)twoButton:(id)sender;
+-(IBAction)threeButton:(id)sender;
+-(IBAction)fourButton:(id)sender;
+-(IBAction)fiveButton:(id)sender;
+-(IBAction)sixButton:(id)sender;
+-(IBAction)sevenButton:(id)sender;
+-(IBAction)eightButton:(id)sender;
+-(IBAction)nineButton:(id)sender;
+
+-(IBAction)decimalButton:(id)sender;
+
+-(IBAction)acButton:(id)sender;
+
+-(IBAction)masButton:(id)sender;
+-(IBAction)menosButton:(id)sender;
+-(IBAction)porButton:(id)sender;
+-(IBAction)entreButton:(id)sender;
+
+-(IBAction)equalButton:(id)sender;
+
 
 @end
--- ArboCalculadora/ViewController.m
+++ ArboCalculadora/ViewController.m
@@ -13,6 +13,7 @@
 @end
 
 @implementation ViewController
+@synthesize calculadoraText=calculadoraText_;
 
 - (void)viewDidLoad
 {
@@ -24,6 +25,78 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+-(void)concateno:(int)numero{
+    if([calculadoraText_.text isEqual:@"0"]){
+        if(numero!=0)
+            calculadoraText_.text = [NSString stringWithFormat:@"%d", numero];
+    }else{
+        
+        calculadoraText_.text =   [calculadoraText_.text stringByAppendingString:[NSString stringWithFormat:@"%d", numero] ];
+    }
+}
+
+
+-(IBAction)zeroButton:(id)sender{
+    /*calculadoraText_.text = [NSString stringWithFormat:@"%.f",[calculadoraText_.text doubleValue] +1];*/
+    
+    [self concateno:0];
+
+}
+-(IBAction)oneButton:(id)sender{
+    
+    [self concateno:1];
+}
+-(IBAction)twoButton:(id)sender{
+    [self concateno:2];
+}
+-(IBAction)threeButton:(id)sender{
+    [self concateno:3];
+}
+-(IBAction)fourButton:(id)sender{
+    [self concateno:4];
+}
+-(IBAction)fiveButton:(id)sender{
+    [self concateno:5];
+}
+-(IBAction)sixButton:(id)sender{
+    [self concateno:6];
+}
+-(IBAction)sevenButton:(id)sender{
+    [self concateno:7];
+}
+-(IBAction)eightButton:(id)sender{
+    [self concateno:8];
+}
+-(IBAction)nineButton:(id)sender{
+    [self concateno:9];
+}
+
+-(IBAction)decimalButton:(id)sender{
+    
+}
+
+-(IBAction)acButton:(id)sender{
+    
+}
+
+-(IBAction)masButton:(id)sender{
+    
+}
+-(IBAction)menosButton:(id)sender{
+    
+}
+-(IBAction)porButton:(id)sender{
+    
+}
+-(IBAction)entreButton:(id)sender{
+    
+}
+
+-(IBAction)equalButton:(id)sender{
+    
 }
 
 @end
